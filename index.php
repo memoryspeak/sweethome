1703c54371a5ee0fc96125d72e088766c77d8bb0
nuxahost <iliya.abdrakhimov@gmail.com>
2022-02-22 09:43:53 +0500

    create sweethome and insert bootstrap files

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+?>
+
+<!doctype html>
+<html lang="ru">
+    <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Платформа семейного быта, домашняя социальная сеть Sweet&#128512;&#128149;Home": семейные фото, расписания, календарь, документы, аналитика расходов, генеалогическое дерево...>
+	<meta name="author" content="@memoryspeak, iliya.abdrakhimov@gmail.com">
+	<meta name="generator" content="Bootstrap v5.0">
+	<title>Sweet&#128512;&#128149;Home</title>
+	<link rel="shortcut icon" href="favicon.png" type="image/png">
+	<link rel="stylesheet" href="assets/dist/css/bootstrap.min.css">
+    </head>
+</html>
